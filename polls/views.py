af2ab71ffb80fff4d1cef8d986a8314dcab2deaf
Rahul Sharma <rahulsharmacs50@gmail.com>
2019-07-04 16:03:21 +0530

    style code added

--- polls/views.py
+++ polls/views.py
@@ -5,34 +5,6 @@ import time
 import cv2
 
 # Create your views here.
-
-def index(request):
-	
-	return render(request, "home/index.html")
-
-def generateStyle(request):
-	
-	contentImageFromPage = request.GET.get('contentImage')
-	styleImageFromPage = request.GET.get('styleImage')
-	contentImageLocation = "static/images/content_images/" + contentImageFromPage
-	styleImageLocation = "static/images/style_images/" + styleImageFromPage
-
-	contentImage = cv2.imread(contentImageLocation)
-	styleImage = cv2.imread(styleImageLocation)
-
-	contentImageGrayScale = cv2.cvtColor(contentImage, cv2.COLOR_BGR2GRAY)
-	saveImageLocation = "static/images/generated_images/" + contentImageFromPage + "_gray.jpg"
-
-	cv2.imwrite(saveImageLocation, contentImageGrayScale)
-
-	template_code = """<!doctype html><head><title>Your art image</title></head><body><img src = {{ saveImageLocation }}/> </body></html>"""
-
-	template = engines['django'].from_string(template_code)
-
-	time.sleep(5)
-
-	return HttpResponse(template.render(context={'saveImageLocation': saveImageLocation}))
-
 	
 def build_model(input_img):
   if args.verbose: print('\nBUILDING VGG-19 NETWORK')
@@ -232,3 +204,399 @@ def sum_content_losses(sess, net, content_img):
     content_loss += content_layer_loss(p, x) * weight
   content_loss /= float(len(args.content_layers))
   return content_loss
+
+def read_image(path):
+  # bgr image
+  img = cv2.imread(path, cv2.IMREAD_COLOR)
+  check_image(img, path)
+  img = img.astype(np.float32)
+  img = preprocess(img)
+  return img
+
+def write_image(path, img):
+  img = postprocess(img)
+  cv2.imwrite(path, img)
+
+def preprocess(img):
+  imgpre = np.copy(img)
+  # bgr to rgb
+  imgpre = imgpre[...,::-1]
+  # shape (h, w, d) to (1, h, w, d)
+  imgpre = imgpre[np.newaxis,:,:,:]
+  imgpre -= np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+  return imgpre
+
+def postprocess(img):
+  imgpost = np.copy(img)
+  imgpost += np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+  # shape (1, h, w, d) to (h, w, d)
+  imgpost = imgpost[0]
+  imgpost = np.clip(imgpost, 0, 255).astype('uint8')
+  # rgb to bgr
+  imgpost = imgpost[...,::-1]
+  return imgpost
+
+def read_flow_file(path):
+  with open(path, 'rb') as f:
+    # 4 bytes header
+    header = struct.unpack('4s', f.read(4))[0]
+    # 4 bytes width, height    
+    w = struct.unpack('i', f.read(4))[0]
+    h = struct.unpack('i', f.read(4))[0]   
+    flow = np.ndarray((2, h, w), dtype=np.float32)
+    for y in range(h):
+      for x in range(w):
+        flow[0,y,x] = struct.unpack('f', f.read(4))[0]
+        flow[1,y,x] = struct.unpack('f', f.read(4))[0]
+  return flow
+
+def read_weights_file(path):
+  lines = open(path).readlines()
+  header = list(map(int, lines[0].split(' ')))
+  w = header[0]
+  h = header[1]
+  vals = np.zeros((h, w), dtype=np.float32)
+  for i in range(1, len(lines)):
+    line = lines[i].rstrip().split(' ')
+    vals[i-1] = np.array(list(map(np.float32, line)))
+    vals[i-1] = list(map(lambda x: 0. if x < 255. else 1., vals[i-1]))
+  # expand to 3 channels
+  weights = np.dstack([vals.astype(np.float32)] * 3)
+  return weights
+
+def normalize(weights):
+  denom = sum(weights)
+  if denom > 0.:
+    return [float(i) / denom for i in weights]
+  else: return [0.] * len(weights)
+
+def maybe_make_directory(dir_path):
+  if not os.path.exists(dir_path):  
+    os.makedirs(dir_path)
+
+def check_image(img, path):
+  if img is None:
+    raise OSError(errno.ENOENT, "No such file", path)
+
+'''
+  rendering -- where the magic happens
+'''
+def stylize(content_img, style_imgs, init_img, frame=None):
+  with tf.device(args.device), tf.Session() as sess:
+    # setup network
+    net = build_model(content_img)
+    
+    # style loss
+    if args.style_mask:
+      L_style = sum_masked_style_losses(sess, net, style_imgs)
+    else:
+      L_style = sum_style_losses(sess, net, style_imgs)
+    
+    # content loss
+    L_content = sum_content_losses(sess, net, content_img)
+    
+    # denoising loss
+    L_tv = tf.image.total_variation(net['input'])
+    
+    # loss weights
+    alpha = args.content_weight
+    beta  = args.style_weight
+    theta = args.tv_weight
+    
+    # total loss
+    L_total  = alpha * L_content
+    L_total += beta  * L_style
+    L_total += theta * L_tv
+    
+    # video temporal loss
+    if args.video and frame > 1:
+      gamma      = args.temporal_weight
+      L_temporal = sum_shortterm_temporal_losses(sess, net, frame, init_img)
+      L_total   += gamma * L_temporal
+
+    # optimization algorithm
+    optimizer = get_optimizer(L_total)
+
+    if args.optimizer == 'adam':
+      minimize_with_adam(sess, net, optimizer, init_img, L_total)
+    elif args.optimizer == 'lbfgs':
+      minimize_with_lbfgs(sess, net, optimizer, init_img)
+    
+    output_img = sess.run(net['input'])
+    
+    if args.original_colors:
+      output_img = convert_to_original_colors(np.copy(content_img), output_img)
+
+    if args.video:
+      write_video_output(frame, output_img)
+    else:
+      write_image_output(output_img, content_img, style_imgs, init_img)
+
+def minimize_with_lbfgs(sess, net, optimizer, init_img):
+  if args.verbose: print('\nMINIMIZING LOSS USING: L-BFGS OPTIMIZER')
+  init_op = tf.global_variables_initializer()
+  sess.run(init_op)
+  sess.run(net['input'].assign(init_img))
+  optimizer.minimize(sess)
+
+def minimize_with_adam(sess, net, optimizer, init_img, loss):
+  if args.verbose: print('\nMINIMIZING LOSS USING: ADAM OPTIMIZER')
+  train_op = optimizer.minimize(loss)
+  init_op = tf.global_variables_initializer()
+  sess.run(init_op)
+  sess.run(net['input'].assign(init_img))
+  iterations = 0
+  while (iterations < args.max_iterations):
+    sess.run(train_op)
+    if iterations % args.print_iterations == 0 and args.verbose:
+      curr_loss = loss.eval()
+      print("At iterate {}\tf=  {}".format(iterations, curr_loss))
+    iterations += 1
+
+def get_optimizer(loss):
+  print_iterations = args.print_iterations if args.verbose else 0
+  if args.optimizer == 'lbfgs':
+    optimizer = tf.contrib.opt.ScipyOptimizerInterface(
+      loss, method='L-BFGS-B',
+      options={'maxiter': args.max_iterations,
+                  'disp': print_iterations})
+  elif args.optimizer == 'adam':
+    optimizer = tf.train.AdamOptimizer(args.learning_rate)
+  return optimizer
+
+def write_video_output(frame, output_img):
+  fn = args.content_frame_frmt.format(str(frame).zfill(4))
+  path = os.path.join(args.video_output_dir, fn)
+  write_image(path, output_img)
+
+def write_image_output(output_img, content_img, style_imgs, init_img):
+  out_dir = os.path.join(args.img_output_dir, args.img_name)
+  maybe_make_directory(out_dir)
+  img_path = os.path.join(out_dir, args.img_name+'.png')
+  content_path = os.path.join(out_dir, 'content.png')
+  init_path = os.path.join(out_dir, 'init.png')
+
+  write_image(img_path, output_img)
+  write_image(content_path, content_img)
+  write_image(init_path, init_img)
+  index = 0
+  for style_img in style_imgs:
+    path = os.path.join(out_dir, 'style_'+str(index)+'.png')
+    write_image(path, style_img)
+    index += 1
+  
+  # save the configuration settings
+  out_file = os.path.join(out_dir, 'meta_data.txt')
+  f = open(out_file, 'w')
+  f.write('image_name: {}\n'.format(args.img_name))
+  f.write('content: {}\n'.format(args.content_img))
+  index = 0
+  for style_img, weight in zip(args.style_imgs, args.style_imgs_weights):
+    f.write('styles['+str(index)+']: {} * {}\n'.format(weight, style_img))
+    index += 1
+  index = 0
+  if args.style_mask_imgs is not None:
+    for mask in args.style_mask_imgs:
+      f.write('style_masks['+str(index)+']: {}\n'.format(mask))
+      index += 1
+  f.write('init_type: {}\n'.format(args.init_img_type))
+  f.write('content_weight: {}\n'.format(args.content_weight))
+  f.write('style_weight: {}\n'.format(args.style_weight))
+  f.write('tv_weight: {}\n'.format(args.tv_weight))
+  f.write('content_layers: {}\n'.format(args.content_layers))
+  f.write('style_layers: {}\n'.format(args.style_layers))
+  f.write('optimizer_type: {}\n'.format(args.optimizer))
+  f.write('max_iterations: {}\n'.format(args.max_iterations))
+  f.write('max_image_size: {}\n'.format(args.max_size))
+  f.close()
+
+'''
+image loading and processing
+'''
+
+def get_init_image(init_type, content_img, style_imgs, frame=None):
+  if init_type == 'content':
+    return content_img
+  elif init_type == 'style':
+    return style_imgs[0]
+  elif init_type == 'random':
+    init_img = get_noise_image(args.noise_ratio, content_img)
+    return init_img
+  # only for video frames
+  elif init_type == 'prev':
+    init_img = get_prev_frame(frame)
+    return init_img
+  elif init_type == 'prev_warped':
+    init_img = get_prev_warped_frame(frame)
+    return init_img
+
+def get_content_image(content_img):
+  path = os.path.join(args.content_img_dir, content_img)
+   # bgr image
+  img = cv2.imread(path, cv2.IMREAD_COLOR)
+  check_image(img, path)
+  img = img.astype(np.float32)
+  h, w, d = img.shape
+  mx = args.max_size
+  # resize if > max size
+  if h > w and h > mx:
+    w = (float(mx) / float(h)) * w
+    img = cv2.resize(img, dsize=(int(w), mx), interpolation=cv2.INTER_AREA)
+  if w > mx:
+    h = (float(mx) / float(w)) * h
+    img = cv2.resize(img, dsize=(mx, int(h)), interpolation=cv2.INTER_AREA)
+  img = preprocess(img)
+  return img
+
+def get_style_images(content_img):
+  _, ch, cw, cd = content_img.shape
+  style_imgs = []
+  for style_fn in args.style_imgs:
+    path = os.path.join(args.style_imgs_dir, style_fn)
+    # bgr image
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    check_image(img, path)
+    img = img.astype(np.float32)
+    img = cv2.resize(img, dsize=(cw, ch), interpolation=cv2.INTER_AREA)
+    img = preprocess(img)
+    style_imgs.append(img)
+  return style_imgs
+
+def get_noise_image(noise_ratio, content_img):
+  np.random.seed(args.seed)
+  noise_img = np.random.uniform(-20., 20., content_img.shape).astype(np.float32)
+  img = noise_ratio * noise_img + (1.-noise_ratio) * content_img
+  return img
+
+def get_mask_image(mask_img, width, height):
+  path = os.path.join(args.content_img_dir, mask_img)
+  img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+  check_image(img, path)
+  img = cv2.resize(img, dsize=(width, height), interpolation=cv2.INTER_AREA)
+  img = img.astype(np.float32)
+  mx = np.amax(img)
+  img /= mx
+  return img
+
+def warp_image(src, flow):
+  _, h, w = flow.shape
+  flow_map = np.zeros(flow.shape, dtype=np.float32)
+  for y in range(h):
+    flow_map[1,y,:] = float(y) + flow[1,y,:]
+  for x in range(w):
+    flow_map[0,:,x] = float(x) + flow[0,:,x]
+  # remap pixels to optical flow
+  dst = cv2.remap(
+    src, flow_map[0], flow_map[1], 
+    interpolation=cv2.INTER_CUBIC, borderMode=cv2.BORDER_TRANSPARENT)
+  return dst
+
+def convert_to_original_colors(content_img, stylized_img):
+  content_img  = postprocess(content_img)
+  stylized_img = postprocess(stylized_img)
+  if args.color_convert_type == 'yuv':
+    cvt_type = cv2.COLOR_BGR2YUV
+    inv_cvt_type = cv2.COLOR_YUV2BGR
+  elif args.color_convert_type == 'ycrcb':
+    cvt_type = cv2.COLOR_BGR2YCR_CB
+    inv_cvt_type = cv2.COLOR_YCR_CB2BGR
+  elif args.color_convert_type == 'luv':
+    cvt_type = cv2.COLOR_BGR2LUV
+    inv_cvt_type = cv2.COLOR_LUV2BGR
+  elif args.color_convert_type == 'lab':
+    cvt_type = cv2.COLOR_BGR2LAB
+    inv_cvt_type = cv2.COLOR_LAB2BGR
+  content_cvt = cv2.cvtColor(content_img, cvt_type)
+  stylized_cvt = cv2.cvtColor(stylized_img, cvt_type)
+  c1, _, _ = cv2.split(stylized_cvt)
+  _, c2, c3 = cv2.split(content_cvt)
+  merged = cv2.merge((c1, c2, c3))
+  dst = cv2.cvtColor(merged, inv_cvt_type).astype(np.float32)
+  dst = preprocess(dst)
+  return dst
+
+def render_single_image():
+  content_img = get_content_image(args.content_img)
+  style_imgs = get_style_images(content_img)
+  with tf.Graph().as_default():
+    print('\n---- RENDERING SINGLE IMAGE ----\n')
+    init_img = get_init_image(args.init_img_type, content_img, style_imgs)
+    tick = time.time()
+    stylize(content_img, style_imgs, init_img)
+    tock = time.time()
+    print('Single image elapsed time: {}'.format(tock - tick))
+
+def warp_image(src, flow):
+  _, h, w = flow.shape
+  flow_map = np.zeros(flow.shape, dtype=np.float32)
+  for y in range(h):
+    flow_map[1,y,:] = float(y) + flow[1,y,:]
+  for x in range(w):
+    flow_map[0,:,x] = float(x) + flow[0,:,x]
+  # remap pixels to optical flow
+  dst = cv2.remap(
+    src, flow_map[0], flow_map[1], 
+    interpolation=cv2.INTER_CUBIC, borderMode=cv2.BORDER_TRANSPARENT)
+  return dst
+
+def convert_to_original_colors(content_img, stylized_img):
+  content_img  = postprocess(content_img)
+  stylized_img = postprocess(stylized_img)
+  if args.color_convert_type == 'yuv':
+    cvt_type = cv2.COLOR_BGR2YUV
+    inv_cvt_type = cv2.COLOR_YUV2BGR
+  elif args.color_convert_type == 'ycrcb':
+    cvt_type = cv2.COLOR_BGR2YCR_CB
+    inv_cvt_type = cv2.COLOR_YCR_CB2BGR
+  elif args.color_convert_type == 'luv':
+    cvt_type = cv2.COLOR_BGR2LUV
+    inv_cvt_type = cv2.COLOR_LUV2BGR
+  elif args.color_convert_type == 'lab':
+    cvt_type = cv2.COLOR_BGR2LAB
+    inv_cvt_type = cv2.COLOR_LAB2BGR
+  content_cvt = cv2.cvtColor(content_img, cvt_type)
+  stylized_cvt = cv2.cvtColor(stylized_img, cvt_type)
+  c1, _, _ = cv2.split(stylized_cvt)
+  _, c2, c3 = cv2.split(content_cvt)
+  merged = cv2.merge((c1, c2, c3))
+  dst = cv2.cvtColor(merged, inv_cvt_type).astype(np.float32)
+  dst = preprocess(dst)
+  return dst
+
+def render_single_image():
+  content_img = get_content_image(args.content_img)
+  style_imgs = get_style_images(content_img)
+  with tf.Graph().as_default():
+    print('\n---- RENDERING SINGLE IMAGE ----\n')
+    init_img = get_init_image(args.init_img_type, content_img, style_imgs)
+    tick = time.time()
+    stylize(content_img, style_imgs, init_img)
+    tock = time.time()
+    print('Single image elapsed time: {}'.format(tock - tick))
+
+def index(request):
+	
+	return render(request, "home/index.html")
+
+def generateStyle(request):
+	
+	contentImageFromPage = request.GET.get('contentImage')
+	styleImageFromPage = request.GET.get('styleImage')
+	contentImageLocation = "static/images/content_images/" + contentImageFromPage
+	styleImageLocation = "static/images/style_images/" + styleImageFromPage
+
+	contentImage = cv2.imread(contentImageLocation)
+	styleImage = cv2.imread(styleImageLocation)
+
+	contentImageGrayScale = cv2.cvtColor(contentImage, cv2.COLOR_BGR2GRAY)
+	saveImageLocation = "static/images/generated_images/" + contentImageFromPage + "_gray.jpg"
+
+	cv2.imwrite(saveImageLocation, contentImageGrayScale)
+
+	template_code = """<!doctype html><head><title>Your art image</title></head><body><img src = {{ saveImageLocation }}/> </body></html>"""
+
+	template = engines['django'].from_string(template_code)
+
+	time.sleep(5)
+
+	return HttpResponse(template.render(context={'saveImageLocation': saveImageLocation}))
