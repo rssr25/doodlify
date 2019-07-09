f565bd67a5bb39ec4beb89ba1647339f6285e0cc
Rahul Sharma <rahulsharmacs50@gmail.com>
2019-07-09 13:42:57 +0530

    fifth push

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,4 +128,4 @@ STATICFILES_DIRS = [
 ]
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import polls
+
 
 urlpatterns = [
     path('', include('polls.urls')),
@@ -23,3 +25,5 @@ urlpatterns = [
     path('getStyleUrl', polls.views.generateStyle),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
